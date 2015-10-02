1414d8fae65f8e360a11fcfed42ff7861ea79ec2
peterortiz82 <peterortiz82@gmail.com>
2015-10-02 11:34:19 -0400

    made some changes for better ux

--- app/controllers/common_errors_controller.rb
+++ app/controllers/common_errors_controller.rb
@@ -2,34 +2,27 @@ class CommonErrorsController < ApplicationController
   before_action :all_common_errors, only: [:index, :preview]
   before_action :set_common_error, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  impressionist :actions=>[:show]
+  before_action :authenticate_user!, except: [:index, :show, :preview]
+  impressionist actions: [:show]
 
-  
-  
   def index
     @common_errors = CommonError.paginate(page: params[:page], per_page: 12)
   end
 
- 
   def show
-   impressionist(@common_error) 
+    impressionist(@common_error)
   end
 
   def preview
   end
 
- 
   def new
     @common_error = current_user.common_errors.build
   end
 
-  
   def edit
   end
 
-  
-  
   def create
     @common_error = CommonError.new(common_error_params)
     @common_error.user_id = current_user.id
@@ -44,8 +37,6 @@ class CommonErrorsController < ApplicationController
     end
   end
 
-  
-  
   def update
     respond_to do |format|
       if @common_error.update(common_error_params)
@@ -58,33 +49,30 @@ class CommonErrorsController < ApplicationController
     end
   end
 
-  
-  
   def destroy
     @common_error.destroy
     respond_to do |format|
       format.html { redirect_to common_errors_url, notice: 'Common error was successfully destroyed.' }
       format.json { head :no_content }
     end
-  end  
+  end
 
   private
 
-    def correct_user
-      @common_error = current_user.common_errors.find_by(params[:id])
-      redirect_to common_errors_path, notice: "Not authorized to edit this post" if @common_error.nil?
-    end
+  def correct_user
+    @common_error = current_user.common_errors.find_by(params[:id])
+    redirect_to common_errors_path, notice: "Not authorized to edit this post" if @common_error.nil?
+  end
 
-    def all_common_errors
-      @common_errors = CommonError.all.order("created_at DESC")
-    end
-  
-    def set_common_error
-      @common_error = CommonError.find(params[:id])
-    end
+  def all_common_errors
+    @common_errors = CommonError.all.order("created_at DESC")
+  end
 
-  
-    def common_error_params
-      params.require(:common_error).permit(:image, :error_msg, :description, :user_id)
-    end
+  def set_common_error
+    @common_error = CommonError.find(params[:id])
+  end
+
+  def common_error_params
+    params.require(:common_error).permit(:image, :error_msg, :description, :user_id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150224012908) do
+ActiveRecord::Schema.define(version: 20150224005738) do
 
   create_table "common_errors", force: :cascade do |t|
     t.string   "error_msg"
